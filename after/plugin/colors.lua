24309d70864739b850b9a19479b61ac1181d4b19
sakresh <sarkeshnandu@gmail.com>
2023-12-05 12:43:07 +0530

    Remaps updated

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,7 +1,6 @@
 function Colors(color)
 	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
-
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>g', builtin.git_files, {})
-vim.keymap.set('n', '<leader>s', function()
+vim.keymap.set('n', '<leader>gs', builtin.git_files, {})
+vim.keymap.set('n', '<leader>g', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)

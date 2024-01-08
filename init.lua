4b9cc4bd4f0b00faa07a1d32a4337670c03c2359
sakresh <sarkeshnandu@gmail.com>
2024-01-08 09:16:28 +0530

    Config added for NEOVIDE

--- init.lua
+++ init.lua
@@ -1,1 +1,14 @@
 require("neovortex")
+
+-- Neovide config
+
+if vim.g.neovide then
+    vim.o.guifont = "JetBrains Mono:h9"
+    vim.g.neovide_transparency = 0.8
+    vim.g.transparency = 0.8
+    vim.g.neovide_padding_top = 6
+    vim.g.neovide_padding_bottom = 6
+    vim.g.neovide_padding_right = 6
+    vim.g.neovide_padding_left = 6
+end
+

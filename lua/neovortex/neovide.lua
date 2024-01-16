55de972dd20f35028f111ba0ab54198001c76e7d
sakresh <sarkeshnandu@gmail.com>
2024-01-16 18:57:58 +0530

    Function added for telescope to read hidden files

--- lua/neovortex/neovide.lua
+++ lua/neovortex/neovide.lua
@@ -1,11 +1,15 @@
 -- Config only for neovim graphical clinet
 if vim.g.neovide then
-    vim.o.guifont = "JetBrains Mono:h9"
+    vim.o.guifont = "JetBrains Mono:h10"
     vim.g.neovide_transparency = 0.8
     vim.g.transparency = 0.8
     vim.g.neovide_padding_top = 6
     vim.g.neovide_padding_bottom = 6
     vim.g.neovide_padding_right = 6
     vim.g.neovide_padding_left = 6
+    vim.keymap.set("n", "<C-S-c>", "\"+y")
+    vim.keymap.set("v", "<C-S-c>", "\"+y")
+    vim.keymap.set("n", "<C-S-v>", "\"+p")
+    vim.keymap.set("v", "<C-S-v>", "\"+p")
 end
 

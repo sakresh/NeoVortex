b23e2ab60fc6d03a3473e683eb6aeb6857bb68c3
name <sarkeshnandu@gmail.com>
2024-02-26 09:24:14 +0530

    Keymap in harpoon changed

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,10 +1,10 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+local Sakresh_fugitive = vim.api.nvim_create_augroup("Sakresh_fugitive", {})
 
 local autocmd = vim.api.nvim_create_autocmd
 autocmd("BufWinEnter", {
-    group = ThePrimeagen_Fugitive,
+    group = Sakresh_fugitive,
     pattern = "*",
     callback = function()
         if vim.bo.ft ~= "fugitive" then
--- lua/neovortex/remap.lua
+++ lua/neovortex/remap.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>l", vim.cmd.Ex)
 
--- vim.g.codeium_enabled = false
+vim.g.codeium_enabled = false
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
